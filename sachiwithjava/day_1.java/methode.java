ce760589a52ce126f306d2de1b56dbb7f4746991
Sachin Maurya <kumarsachin67671@gmail.com>
2023-12-07 23:12:52 +0530

    call function

--- sachiwithjava/day_1.java/methode.java
+++ sachiwithjava/day_1.java/methode.java
@@ -1,7 +1,27 @@
-
 public class methode {
+    static int logic(int x,int y){
+        int z;
+        if(x>y){
+            z=x+y;
+        }
+        else{
+            z=(x+y)*5;
+        }
+        return z;
+        
+    }
     public static void main(String[] args) {
-        System.out.println("hello");
+        int a=5;
+        int b=7;
+        int c;
+        c=logic(a,b);
+        int a1 =2;
+        int b1 =1;
+        int c1;
+       c1=logic(a1,b1);
+        
+        System.out.println(c);
+        System.out.println(c1);
     }
     
 }
