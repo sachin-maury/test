6e2fa98a3d7292c18e2a13842069d5ceeb28be1b
Sachin Maurya <kumarsachin67671@gmail.com>
2023-12-09 10:19:14 +0530

    ch8 qn2

---
+++ sachiwithjava/day_3.java/ch8_qn2.java
@@ -0,0 +1,18 @@
+class cellphone{
+    public void ringing(){
+        System.out.println("Ringing..........");
+    }
+    public void vibrating(){
+        System.out.println("Vibrating........");
+    }
+
+}
+public class ch8_qn2 {
+    public static void main(String[] args) {
+        //problem;
+        cellphone nokia =new cellphone();
+        nokia.ringing();
+        nokia.vibrating();
+        
+    }
+}
