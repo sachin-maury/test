5b002fb722b9d34556639fd50afa7b876913719b
Sachin Maurya <kumarsachin67671@gmail.com>
2023-12-09 10:45:04 +0530

    ch8 qn5 games

---
+++ sachiwithjava/day_3.java/qn5_tommyvecitti.java
@@ -0,0 +1,21 @@
+class Tommy{
+    public void hit(){
+        System.out.println("hitting........");
+    }
+    public void run(){
+        System.out.println("running........");
+    }
+    public void fir(){
+        System.out.println("firing.........");
+    }
+
+}
+public class qn5_tommyvecitti {
+    public static void main(String[] args) {
+        //problem:
+        Tommy tm=new Tommy();
+        tm.hit();
+        tm.run();
+        tm.fir();
+    }
+}
