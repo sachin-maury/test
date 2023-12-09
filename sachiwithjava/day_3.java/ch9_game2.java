562d6a3865b4652234e63c0c848c1dd9e2214887
Sachin Maurya <kumarsachin67671@gmail.com>
2023-12-09 21:45:11 +0530

    game ch9

---
+++ sachiwithjava/day_3.java/ch9_game2.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class ch9_game2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter 0 for Rock,1 for Paper,2 for Scissor");
+        int userInput = sc.nextInt();
+        Random random = new Random();
+        int computerInput = random.nextInt(3);
+        if(userInput == computerInput){
+            System.out.println("Draw");
+        }
+        else if(userInput == 0 && computerInput == 2 || userInput == 1 && computerInput == 0 ||userInput == 2 && computerInput == 1)
+{
+    System.out.println("You win!");
+}  
+else{
+    System.out.println("Computer win!");
+}
+System.out.println("Computeer choice:" + computerInput);
+}
+}
